24c9b46efb639602bebd1a792b5066f44c009f94
sergioleonurrutia <3250762511401@ingenieria.usac.edu.gt>
2020-06-09 12:41:16 -0600

    Falto el archivo y los enters

--- corto1/corto1.py
+++ corto1/corto1.py
@@ -3,20 +3,22 @@ Numero=5
 Lista=[]
 bandera=True
 cont=0
-
-for i in range(2,Numero):
+archivo = open('collatz.txt', 'a')          #Creo el archivo con el nombre collatz.txt en modo append
+for i in range(2,Numero):                   #For de 2 hasta 722
     print(i)
     cont=i
-    while cont != 1:
-        modulo=cont%2
-        if modulo==0:
-            Lista.append(cont)
+    while cont != 1:                        #Se ejecuta mientras el contador sea diferente de 0
+        modulo=cont%2                       #Determina el modulo para saber si es par
+        if modulo==0:                       #Si es par cont = cont/2
+            Lista.append(cont)              #Escribe al final del archivo
             cont=cont/2
-        else:
+        else:                               #Si es impar cont = (3*cont)+1
             Lista.append(cont)
-            cont =(3*cont)+1
+            cont =(3*cont)+1                #Escribe al final del archivo
         if cont == 1:
             Lista.append(1)
-    print(Lista)
+        #archivo.write(Lista)                    #Escribe la lista en en el archivo
+        print(Lista)
+archivo.close()
 
 
