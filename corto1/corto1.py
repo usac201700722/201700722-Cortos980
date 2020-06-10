d4b896aeb3083adcdd71d97e04001420ec4c5594
sergioleonurrutia <3250762511401@ingenieria.usac.edu.gt>
2020-06-09 18:48:38 -0600

    "Arregle el corto1"

--- corto1/corto1.py
+++ corto1/corto1.py
@@ -1,27 +1,17 @@
-import time #Para generar pausa
-import datetime #Para generar fecha/hora actual
 #Mi carnet es 201700722
 Numero=722
-Lista=[]
-bandera=True
-cont=0
-archivo = open('collatz.txt', 'a')          #Creo el archivo con el nombre collatz.txt en modo append
-for i in range(2,Numero):                   #For de 2 hasta 722
-    print(i)
-    cont=i
-    while cont != 1:                        #Se ejecuta mientras el contador sea diferente de 0
+archivo = open('/home/urrutia/Escritorio/201700722-Cortos980/corto1/collatz.txt', 'w')
+for cont in range(2,Numero):
+    dimension2=[]
+    while cont!=1:
         modulo=cont%2                       #Determina el modulo para saber si es par
         if modulo==0:                       #Si es par cont = cont/2
-            Lista.append(cont)              #Escribe al final del archivo
-            cont=cont/2
+            dimension2.append(cont)              #Escribe al final del archivo
+            cont=cont//2
         else:                               #Si es impar cont = (3*cont)+1
-            Lista.append(cont)
+            dimension2.append(cont)
             cont =(3*cont)+1                #Escribe al final del archivo
         if cont == 1:
-            Lista.append(1)
-        #archivo.write(Lista)                    #Escribe la lista en en el archivo
-        print(Lista)
-    archivo.write(str(Lista))
+            dimension2.append(1)
+    archivo.write(str(dimension2)+'\n')
 archivo.close()
-
-
